27d700711d1c05eb2f7882bac6239e095b89af96
Leon <68347090+LadeShootH@users.noreply.github.com>
2025-10-10 19:51:01 +0200

    events.js aktualisieren

--- testseite/assets/events.js
+++ testseite/assets/events.js
@@ -11,9 +11,9 @@ window.EVENTS = [
     music: "Partymix & Halloween Sounds",
     description: "VERTIGO HALLOWEEN PARTY🎃👻",
     longDescription: `VERTIGO HALLOWEEN PARTY🎃👻
-\n \n
+
 Halloween in Forchheim wird legendär! DJ flo.meins bringt euch die besten Beats. Sei dabei und sichere dir jetzt dein Ticket für die größte Halloween Party Forchheims!
-\n \n
+
 Gratis Getränk deiner Wahl bei Verkleidung!🍹`,
     ticketsUrl: "https://vertigo-nights.de/halloween/6",
     cover: ""
@@ -27,13 +27,13 @@ Gratis Getränk deiner Wahl bei Verkleidung!🍹`,
     longDescription: `✨ Vertigo Opening ✨
 
 Am Wochenende war es endlich soweit: Das erste Vertigo Opening hat stattgefunden. Und was sollen wir sagen? Es war ein voller Erfolg und ein Abend, den wir so schnell nicht vergessen werden! 🙌
-\n \n
+
 Von Beginn an herrschte eine besondere Atmosphäre – Vorfreude, Neugier und jede Menge Energie lagen in der Luft. Mit DJ Marvelz und Nørobeatz hatten wir zwei Künstler am Start, die genau wussten, wie man die Menge packt. Mit ihrem Mix aus satten Bässen, treibenden Beats und abwechslungsreichen Sounds haben sie den Abend geprägt und das Publikum auf eine musikalische Reise geschickt. 🎶🔥
-\n \n
+
 Die Stimmung war ausgelassen, die Leute haben gefeiert, gelacht und die Nacht gemeinsam zelebriert. Es war deutlich zu spüren: Forchheim hat auf ein Event wie dieses gewartet. ✨
-\n \n 
+
 Ein riesiges Dankeschön an alle, die gekommen sind, um mit uns die erste Vertigo Night zu erleben. Ihr habt gezeigt, dass hier eine Community entsteht, die zusammen etwas Neues auf die Beine stellt. 🥂🧡
-\n \n 
+
 Wir können es kaum erwarten, bald den nächsten Schritt zu gehen. Dies war nur der Anfang. Vertigo Nights wird weiter wachsen, größer werden und noch mehr besondere Momente schaffen. 🚀`,
     music: "Clubsounds",
     stats: { guests: 85, rating: 4.0 }
@@ -56,15 +56,15 @@ Wir können es kaum erwarten, bald den nächsten Schritt zu gehen. Dies war nur 
     venue: "Kellerwald Winterzauber",
     description: "Silent Disco am Kellerwald",
     longDescription: `❄️ SILENT DISCO beim Kellerwald WINTERZAUBER ❄️
-    \n \n 
-    Am 31. Januar & 01. Februar verwandelte sich der obere Festplatz im Kellerwald in einen technoiden Spielplatz mit mehreren DJs, die feinsten Techno, House und Hip-Hop direkt in euere Ohren zauberten. \n 
-    Auf drei verschiedenen Kanälen spielten mehrere DJs treibende Musik, nur für euch! 🥶 \n 
-    Was wir im Angebot hatten:\n 
-    31.01.2025:\n 
-    18 Uhr: DJ Curse (Tech House)\n 
-    20 Uhr: flo.meins (EDM)\n 
-    01.02.2025:\n 
-    18 Uhr: Alan Oey (Techno)\n 
+
+    Am 31. Januar & 01. Februar verwandelte sich der obere Festplatz im Kellerwald in einen technoiden Spielplatz mit mehreren DJs, die feinsten Techno, House und Hip-Hop direkt in euere Ohren zauberten. 
+    Auf drei verschiedenen Kanälen spielten mehrere DJs treibende Musik, nur für euch! 🥶 
+    Was wir im Angebot hatten:
+    31.01.2025:
+    18 Uhr: DJ Curse (Tech House)
+    20 Uhr: flo.meins (EDM)
+    01.02.2025:
+    18 Uhr: Alan Oey (Techno)
     20 Uhr: DJ YOYI (Trance)`,
     music: "Techno, House & Hip-Hop",
     stats: { guests: 150, rating: 4.2 }
